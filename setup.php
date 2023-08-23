c054cd5f92e8911e26c8f21ef07d9d0a87e5d4e7
Nilesh Gangthade <nileshgangthade7171@gmail.com>
2023-08-23 14:14:04 +0530

    update a table format

--- setup.php
+++ setup.php
@@ -15,24 +15,29 @@ require 'config.php';
 $createMainTableSql = "CREATE TABLE IF NOT EXISTS main_table (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_type VARCHAR(255) NOT NULL,
-    department VARCHAR(255),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    department VARCHAR(255) DEFAULT NULL,
     password VARCHAR(255) NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE,
-    is_approve BOOLEAN DEFAULT FALSE
+    is_approve BOOLEAN DEFAULT FALSE,
+    otp VARCHAR(6) DEFAULT NULL,
+    otp_expiry TIMESTAMP DEFAULT NULL
 )";
 $conn->query($createMainTableSql);
 
 $checkTableSql = "CREATE TABLE IF NOT EXISTS temp_table (
-    user_type VARCHAR(255),
-    department VARCHAR(255),
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255),
-    otp VARCHAR(10),
-    otp_expiry TIMESTAMP,
-    email_status INT DEFAULT 0
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_type VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    department VARCHAR(255) DEFAULT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    email_status INT DEFAULT 0,
+    otp VARCHAR(6) DEFAULT NULL,
+    otp_expiry TIMESTAMP DEFAULT NULL
+    
 )";
 $conn->query($checkTableSql);
 
